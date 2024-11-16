ac394446051fce936cd6f2c62727df285f9df4f7
falahhassan <hakukun30@gmail.com>
2024-11-16 16:06:23 +0300

    first github commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from linecache import getline
 from pynput import keyboard
 
 
@@ -6,13 +7,17 @@ with open("dictionary.json","r") as file:
     data =json.load(file)
 
 while True:
-    
    
    try:
-       user_input = input("Enter Your Word: ").lower()    
-       print(data[user_input])    
+    
+       user_input = input("Enter Your Word: ").lower() 
+       meaning= data[user_input]
+       line = getline(meaning,0)
+       if user_input=="q":
+            print("quiting the program")
+            break
+       else:     
+            print(line)
    except Exception:
        print("Word not exist Try another Word")
-   if keyboard.Events.Press("esc"):
-       print("quiting the program")
-       break
+   
