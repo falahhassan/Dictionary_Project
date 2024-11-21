6e458519cfe150e1d7b08a099fbcd407ed7b426c
falahhassan <hakukun30@gmail.com>
2024-11-21 06:18:19 +0300

    Adding word to dictionary with search counting

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import json
 import useful_functions
 
-
+#opining the dictionary file
 with open("dictionary.json","r") as file:
     data =json.load(file)
+#Variables Global
+word_list =[]
+word_dict ={}
+dict = {}
+
+
+
+#Starting the dictionary    
 print("T H I S  I S  M Y  F I R S T  D I C T I O N A R Y")
 print("=================================================\n\n")
 
@@ -18,7 +26,22 @@ while True:
             break
        else:     
             print(meaning)
+            
    except Exception:
-       print("Word not exist Try another Word Maybe You mean:\n")
+       print("\n\nWord not exist Try another Word Maybe You mean:\n")
        print(useful_functions.close_match(user_input, data))
-   
+
+
+#adding words to dictionary list
+
+   if user_input in data:
+       print("\n\nExist!!!!!!!!!!")
+       word_list.append(user_input)
+       if user_input in word_list:
+
+           word_occurance = word_list.count(user_input)
+           word_dict.update({user_input : word_occurance})
+
+   print(word_list)  
+   print(word_dict)     
+
